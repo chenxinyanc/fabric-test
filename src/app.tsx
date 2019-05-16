198b89c8f838bb78558048301040e9aed43935b5
Xinyan Chen <xinychen@microsoft.com>
2019-05-16 16:50:13 +0800

    Basic fabric UI.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,26 @@
 import * as React from "react";
+import { DefaultButton } from "office-ui-fabric-react/lib/Button";
+import { Customizer } from "office-ui-fabric-react";
+import { FluentCustomizations } from "@uifabric/fluent-theme";
+import { ContextualMenuItemType } from 'office-ui-fabric-react/lib/ContextualMenu';
 
 export class App extends React.Component {
 
     public render() {
-        return (<h1>Test!</h1>);
+        return (<Customizer {...FluentCustomizations}>
+            <h1>Test!</h1>
+            <DefaultButton text="Show menu" menuProps={{
+                shouldFocusOnMount: true, items: [
+                    { key: "header", text: "ContextualMenu", itemType: ContextualMenuItemType.Header },
+                    { key: "item1", text: "Item 1" },
+                    { key: "item2", text: "Item 2" },
+                    { key: "item3", text: "Item 3" },
+                    { key: "divider1", itemType: ContextualMenuItemType.Divider },
+                    { key: "item4", text: "Item 4" },
+                    { key: "item5", text: "Item 5" },
+                ]
+            }} />
+        </Customizer>);
     }
 
 }
